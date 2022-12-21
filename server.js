6cc0cfe85915abd65f8a12ffa10e6db8027d5f72
Chad Bailey <chadbailey@gmail.com>
2022-12-21 13:52:51 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import apicache from "apicache-plus";
+import fetch from "node-fetch";
+
+const app = express();
+
+let cache = apicache.middleware;
+app.use(apicache("2 seconds", () => true));
+
+app.get("*", async (req, res) => {
+  const dailyApiPath = `https://api.daily.co/v1${req.originalUrl}`;
+  console.log(
+    `${new Date().toISOString()} Making Daily API request to: ${dailyApiPath}`
+  );
+  let resp = await fetch(dailyApiPath, {
+    headers: { authorization: req.headers.authorization },
+  });
+  // Don't even bother parsing the JSON; just send it back as-is
+  let t = await resp.text();
+  res.set("content-type", "text/javascript");
+  res.send(t);
+});
+
+app.listen(process.env.PORT || 3069, () => {
+  console.log(`Example app listening on port ${process.env.PORT || 3069}`);
+});
